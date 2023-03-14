a1871a11b420951e10e9a37ff24480561de73070
Ammar moulla <ammarturing@gmail.com>
2023-03-14 09:11:08 +0300

    update profile based GCBV :sparkles:

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name="accounts/change_password.html"), name="password_change"),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="accounts/change_password_done.html"), name="password_change_done"),
+    path('account/', views.ProfileUpdateView.as_view(), name="my_account"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
 from .forms import *
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+from django.contrib.auth.models import User
 # Create your views here.
 def signup(request):
     context = {}
@@ -17,3 +20,13 @@ def signup(request):
         "form": form,
     }
     return render(request, "accounts/signup.html", context=context)
+
+
+class ProfileUpdateView(UpdateView):
+    model = User
+    fields = ("first_name", "last_name", "email")
+    template_name = "accounts/my_account.html"
+    success_url = reverse_lazy("my_account")
+
+    def get_object(self):
+        return self.request.user
